5d2f53c6ea8b53f8373f0efe5df06bdbd3724e52
hcs240000 <hcs240000@utdallas.edu>
2026-04-08 22:03:12 +0000

    haiku edits

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,13 +6,13 @@ function randomNumber(min, max){
 let divs = document.querySelectorAll(".grid-container div");
 
 function randomize() {
-    divs.forEach(function (div){
+    divs.forEach(function (div) {
         let scale = randomNumber(.5, 1.2);
         let translateX = randomNumber(0, 50);
         let translateY = randomNumber(0, 20);
         let rotate = randomNumber(0, 360);
         div.style.transform = `scale(${scale}) translate(${translateX}%, ${translateY}%) rotate(${rotate}deg)`;
-    })
+    });
     console.log(divs.length, "randomize!");
 }
 document.addEventListener("click", randomize);
